330f33e61cc5d54fa3c3e96ee25479e2dbb1487f
Grant Bosworth <grb1300@rit.edu>
2025-10-10 16:40:55 -0400

    Created Project 3 and imported provided code

---
+++ Project3/Debug/Core/Src/SysClock.d
@@ -0,0 +1,13 @@
+Core/Src/SysClock.o: ../Core/Src/SysClock.c ../Core/Inc/SysClock.h \
+ ../Core/Inc/stm32l476xx.h ../Core/Inc/core_cm4.h \
+ ../Core/Inc/cmsis_version.h ../Core/Inc/cmsis_compiler.h \
+ ../Core/Inc/cmsis_gcc.h ../Core/Inc/mpu_armv7.h \
+ ../Core/Inc/system_stm32l4xx.h
+../Core/Inc/SysClock.h:
+../Core/Inc/stm32l476xx.h:
+../Core/Inc/core_cm4.h:
+../Core/Inc/cmsis_version.h:
+../Core/Inc/cmsis_compiler.h:
+../Core/Inc/cmsis_gcc.h:
+../Core/Inc/mpu_armv7.h:
+../Core/Inc/system_stm32l4xx.h:
---
+++ Project3/Release/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.d
@@ -0,0 +1,44 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_i2c.c \
+ ../Core/Inc/stm32l4xx_hal.h ../Core/Inc/stm32l4xx_hal_conf.h \
+ ../Core/Inc/stm32l4xx_hal_rcc.h ../Core/Inc/stm32l4xx_hal_def.h \
+ ../Core/Inc/stm32l476xx.h ../Core/Inc/core_cm4.h \
+ ../Core/Inc/cmsis_version.h ../Core/Inc/cmsis_compiler.h \
+ ../Core/Inc/cmsis_gcc.h ../Core/Inc/mpu_armv7.h \
+ ../Core/Inc/system_stm32l4xx.h ../Core/Inc/stm32l4xx.h \
+ ../Core/Inc/stm32l4xx_hal.h \
+ ../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Core/Inc/stm32l4xx_hal_rcc_ex.h ../Core/Inc/stm32l4xx_hal_gpio.h \
+ ../Core/Inc/stm32l4xx_hal_gpio_ex.h ../Core/Inc/stm32l4xx_hal_dma.h \
+ ../Core/Inc/stm32l4xx_hal_cortex.h ../Core/Inc/stm32l4xx_hal_exti.h \
+ ../Core/Inc/stm32l4xx_hal_flash.h ../Core/Inc/stm32l4xx_hal_flash_ex.h \
+ ../Core/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ ../Core/Inc/stm32l4xx_hal_pwr.h ../Core/Inc/stm32l4xx_hal_pwr_ex.h \
+ ../Core/Inc/stm32l4xx_hal_uart.h ../Core/Inc/stm32l4xx_hal_uart_ex.h
+../Core/Inc/stm32l4xx_hal.h:
+../Core/Inc/stm32l4xx_hal_conf.h:
+../Core/Inc/stm32l4xx_hal_rcc.h:
+../Core/Inc/stm32l4xx_hal_def.h:
+../Core/Inc/stm32l476xx.h:
+../Core/Inc/core_cm4.h:
+../Core/Inc/cmsis_version.h:
+../Core/Inc/cmsis_compiler.h:
+../Core/Inc/cmsis_gcc.h:
+../Core/Inc/mpu_armv7.h:
+../Core/Inc/system_stm32l4xx.h:
+../Core/Inc/stm32l4xx.h:
+../Core/Inc/stm32l4xx_hal.h:
+../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Core/Inc/stm32l4xx_hal_rcc_ex.h:
+../Core/Inc/stm32l4xx_hal_gpio.h:
+../Core/Inc/stm32l4xx_hal_gpio_ex.h:
+../Core/Inc/stm32l4xx_hal_dma.h:
+../Core/Inc/stm32l4xx_hal_cortex.h:
+../Core/Inc/stm32l4xx_hal_exti.h:
+../Core/Inc/stm32l4xx_hal_flash.h:
+../Core/Inc/stm32l4xx_hal_flash_ex.h:
+../Core/Inc/stm32l4xx_hal_flash_ramfunc.h:
+../Core/Inc/stm32l4xx_hal_pwr.h:
+../Core/Inc/stm32l4xx_hal_pwr_ex.h:
+../Core/Inc/stm32l4xx_hal_uart.h:
+../Core/Inc/stm32l4xx_hal_uart_ex.h:
