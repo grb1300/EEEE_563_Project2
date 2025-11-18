c4e1bd6fc47b3f3867fe640262392d47cef01278
Grant Bosworth <grb1300@rit.edu>
2025-11-18 17:11:00 -0500

    Debugged the UART, Error in waveform, and GBD linker issues

---
+++ RTOS-proj4/Core/Inc/SigGen.h
@@ -0,0 +1,47 @@
+/*
+ * sig_gen.h
+ *
+ *  Created on: Nov 11, 2025
+ *      Author: mpari
+ */
+
+// =============================================================================
+// This is a simple signal generator API (uses TIM6 → DMA → DAC1)
+// =============================================================================
+#ifndef INC_SIGGEN_H_
+#define INC_SIGGEN_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// waveform enum
+typedef enum {
+    SIG_WAVE_SINE = 0,
+    SIG_WAVE_SQUARE = 1,
+} sig_wave_t;
+
+// Initialize the generator (Initializes DAC+DMA and configures buffers)
+void SigGen_Init(void);
+
+// Start/Stop the timer that clocks the DMA (starts/stops the output)
+void SigGen_Start(void);
+void SigGen_Stop(void);
+
+// Change both Waveform type and Freq at once (returns false if inputs invalid)
+bool SigGen_Set(sig_wave_t wave, uint32_t fout_hz);
+
+// Parse simple CLI commands like
+// Valid command: <waveform> <freq>
+// Example: "Sine 4000" or "Square 400"
+// Returns true on success. On failure, no change is applied
+bool SigGen_ParseCommand(const char *cmd);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // SIG_GEN_H
+
---
+++ RTOS-proj4/Core/Src/freertos.c
@@ -0,0 +1,218 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+#include "usart.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "SigGen.h"
+#include "7seg.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for CLI */
+osThreadId_t CLIHandle;
+const osThreadAttr_t CLI_attributes = {
+  .name = "CLI",
+  .stack_size = 1024 * 4,
+  .priority = (osPriority_t) osPriorityLow,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+void CLI_task(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* creation of CLI */
+  CLIHandle = osThreadNew(CLI_task, NULL, &CLI_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* USER CODE BEGIN Header_CLI_task */
+/**
+* @brief Function implementing the CLI thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_CLI_task */
+__weak void CLI_task(void *argument)
+{
+  /* USER CODE BEGIN CLI_task */
+  /* Infinite loop */
+	for (;;)
+	{
+	    char rx;
+	    static char cmd[32];        // command line buffer
+	    static uint8_t idx = 0;     // current write index
+
+	    // Receive 1 char with small timeout
+	    if (HAL_UART_Receive(&huart2, (uint8_t *)&rx, 1, 10) == HAL_OK)
+	    {
+	        // echo back whatever we got
+	        HAL_UART_Transmit(&huart2, (uint8_t *)&rx, 1, HAL_MAX_DELAY);
+
+	        // ENTER pressed? (CR or LF)
+	        if (rx == '\r' || rx == '\n')
+	        {
+	            uart_print("\r\n");
+
+	            if (idx > 0)
+	            {
+	                cmd[idx] = '\0';  // null-terminate the command string
+//	                for (int i = 0; cmd[i] != '\0'; i++) {
+//	                	if (cmd[i] == '\b'){
+//	                		//now we want a string that is cmd but with the backspace character and character before backspace removed (only if applicable)
+//	                	}
+//	                }
+	                uint8_t parsed_command_value = SigGen_ParseCommand(cmd);
+	                if (parsed_command_value)
+	                {
+	                    uart_print("OK\r\n");
+	                }
+	                else
+	                {
+	                    uart_print("ERR: use \"sine|square <100-9999>\"\r\n");
+	                }
+	            }
+
+	            // reset buffer for next command
+	            idx = 0;
+	        }
+	        else
+	        {
+	            // normal character → add to command buffer if space
+	            if (idx < sizeof(cmd) - 1)
+	            {
+	                cmd[idx++] = rx;
+	            }
+	            else
+	            {
+	                // buffer full: warn and reset
+	                uart_print("\r\nERR: command too long\r\n");
+	                idx = 0;
+	            }
+	        }
+	    }
+
+	    vTaskDelay(1);   // let other tasks run
+	}
+  }
+  /* USER CODE END CLI_task */
+
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
